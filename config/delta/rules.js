5e0bb8c24bc7b2a24ef6855e5d116db234aeff51
Nordine Bittich <contact@bittich.be>
2025-03-28 15:35:10 +0100

    ldes testing tool

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,17 +1,31 @@
 export default [
-  {
-    match: {
-      subject: {},
+    {
+        match: {
+            subject: {},
+        },
+        callback: {
+            url: "http://resource/.mu/delta",
+            method: "POST",
+        },
+        options: {
+            resourceFormat: "v0.0.1",
+            gracePeriod: 1000,
+            foldEffectiveChanges: true,
+            ignoreFromSelf: true,
+        },
     },
-    callback: {
-      url: "http://resource/.mu/delta",
-      method: "POST",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 1000,
-      foldEffectiveChanges: true,
-      ignoreFromSelf: true,
-    },
-  }
+    {
+        match: {
+            predicate: { type: 'uri', value: 'http://purl.org/dc/terms/isVersionOf' }
+        },
+        callback: {
+            url: "http://make-object/delta",
+            method: "POST"
+        },
+        options: {
+            resourceFormat: "v0.0.1",
+            gracePeriod: 500,
+            ignoreFromSelf: true
+        }
+    }
 ];
--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -2,26 +2,59 @@ defmodule Dispatcher do
   use Matcher
   define_accept_types [
     html: [ "text/html", "application/xhtml+html" ],
-    json: [ "application/json", "application/vnd.api+json" ]
+    json: [ "application/json", "application/vnd.api+json" ],
+    sparql: [ "application/sparql-results+json" ],
+    any: [ "*/*" ]
   ]
+  options "/*path", _ do
+    conn
+    |> Plug.Conn.put_resp_header( "access-control-allow-headers", "content-type,accept" )
+    |> Plug.Conn.put_resp_header( "access-control-allow-methods", "*" )
+    |> send_resp( 200, "{ \"message\": \"ok\" }" )
+  end
+  define_layers [ :static, :sparql, :api_services, :frontend_fallback, :resources, :not_found ]
 
-  @any %{}
-  @json %{ accept: %{ json: true } }
-  @html %{ accept: %{ html: true } }
+  # frontend
+  match "/index.html", %{ layer: :static } do
+    forward conn, [], "http://frontend/index.html"
+  end
 
-  define_layers [ :static, :services, :fall_back, :not_found ]
+  get "/assets/*path",  %{ layer: :static } do
+    forward conn, path, "http://frontend/assets/"
+  end
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
-  # Run `docker-compose restart dispatcher` after updating
-  # this file.
+  get "/@appuniversum/*path", %{ layer: :static} do
+    forward conn, path, "http://frontend/@appuniversum/"
+  end
 
-  match "/*_", %{ layer: :not_found } do
-    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+
+  ###############
+  # SPARQL
+  ###############
+  match "/sparql", %{ layer: :sparql, accept: %{ sparql: true} } do
+    forward conn, [], "http://database:8890/sparql"
+  end
+
+
+  ##############################################
+  # METIS
+  ##############################################
+
+  get "/uri-info/*path", %{ accept: %{ json: true } } do
+    forward conn, path, "http://uri-info/"
+  end
+
+  get "/resource-labels/*path", %{ accept: %{ json: true } } do
+    forward conn, path, "http://resource-labels-cache/"
+  end
+
+  get "/resource-labels/*path" do
+    Proxy.forward conn, path, "http://resource-labels/"
+  end
+
+
+  
+  match "/*path", %{ layer: :frontend_fallback, accept: %{ html: true } } do
+    forward conn, [], "http://frontend/index.html"
   end
 end
